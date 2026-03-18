4401fe1b644e9370d89af37439905f197e40140f
SmokeyRGB <mczockerlp@web.de>
2026-03-18 12:30:50 +0100

    Added YouTube Tab-Title Notification Blocker

---
+++ YouTube Notification Wall.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         YouTube Notification Wall
+// @namespace    http://violentmonkey.github.io/
+// @version      0.1
+// @description  Removes the notification count from the YouTube title
+// @author       SmokeyRGB
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    (new MutationObserver(check)).observe(document, {childList: true, subtree: true});
+
+    function check(changes, observer) {
+        if(document.querySelector('.yt-spec-icon-badge-shape__badge')) {
+            //observer.disconnect();
+
+        // Get the current website title
+        const currentTitle = document.title;
+
+        //console.log("YouTube Notification Wall - Title: " + currentTitle);
+
+        // Use a regular expression to find and capture the pattern at the beginning of the title
+        const match = currentTitle.match(/^\(\d+\)(.+)/);
+
+        if (match) {
+            console.log("YouTube Notification Wall - Found pattern: " + match[0]);
+            // Extract the captured part of the title (excluding the pattern)
+            const newTitle = match[1];
+
+            // Set the website title to the extracted title
+            document.title = newTitle;
+}
+        }
+    }
+
+})();
